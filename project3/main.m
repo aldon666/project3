671da7b9ab261b9954232ad7d74cb7836a225486
lanou3g <505680300@qq.com>
2016-06-13 10:00:56 +0800

    我更改了一个东西

---
+++ project3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  project3
+//
+//  Created by lanou3g on 16/6/13.
+//  Copyright © 2016年 Aldon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
